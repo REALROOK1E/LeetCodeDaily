ff4508a1e68400451fa2bc2c666c4a843f9fe03d
zekli883 <zekli883@ad.liu.se>
2025-02-25 04:13:09 +0100

    slidewindow and keep going

---
+++ src/Bitree/Codec.java
@@ -0,0 +1,33 @@
+package Bitree;
+
+/**
+ * @author: ZeKai
+ * @date: 2025/2/21
+ * @description:
+ **/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+return null;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+return null;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
--- src/Bitree/minDepth.java
+++ src/Bitree/minDepth.java
@@ -20,4 +20,12 @@ public class minDepth {
         }
         return Math.max(left,right) + 1;
     }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1,
+                new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                new TreeNode(3, null, new TreeNode(6)));
+
+        System.out.println(root.toString());
+    }
 }
