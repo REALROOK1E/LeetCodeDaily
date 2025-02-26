8edece9a6790d27696041cd3eda71d7b9d3585eb
zekli883 <zekli883@ad.liu.se>
2025-02-26 21:03:30 +0100

    slidewindow

---
+++ src/slidingwindow/MonotonicQueue/shortestsubarray.java
@@ -0,0 +1,13 @@
+package slidingwindow.MonotonicQueue;
+
+/**
+ * @author: ZeKai
+ * @date: 2025/2/26
+ * @description:
+ **/
+public class shortestsubarray {
+    public int shortestSubarray(int[] nums, int k) {
+    //为什么不可以用传统的滑动窗口：因为数组里面有负数，这样数组往右移动，数组的和不一定增加，这时候就要用前缀和数组+滑动窗口了
+        return 0;
+    }
+}
